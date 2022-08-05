7b787d2b08b574fddf4ca934e175709888a8756f
BlackSquareShip <danilg2002@mail.ru>
2022-08-05 19:32:09 +0500

    ready task

--- index.php
+++ index.php
@@ -3,13 +3,20 @@ $conn = new PDO("mysql:host=127.0.0.1;dbname=dbuser", "root", "");
 
 class DBPeople
 {
+    /** Класс DBPeople взаимодействует с базой данных 
+     * (в данном случае таблица user).
+     * Класс может добавлять, удалять пользователей, 
+     * а так же преобразовать дату рождения в возраст,
+     * а так же булевый тип в пол человека.
+     * Данный класс взаимодействует с классом PeopleList.
+     */
     private $id, $name, $surname, $birth_date, $gender, $birth_city;
 
     public function __construct($id = null, $name = null, $surname = null, $birth_date = null, $gender = null, $birth_city = null)
     {
         $this->id = $id;
         $this->conn = new PDO("mysql:host=127.0.0.1;dbname=dbuser", "root", "");
-        if($id != null){
+        if ($id != null) {
             $sql = "SELECT * FROM `user` WHERE `user_id` = $id";
             $people = $this->conn->query($sql);
             $row = $people->fetch();
@@ -19,14 +26,14 @@ class DBPeople
             $this->gender = $row['gender'];
             $this->birth_city = $row['birth_city'];
         }
-        else{
+        else {
             $sql = "INSERT INTO `user`(`name`, `surname`, `birth_date`, `gender`, `birth_city`) 
             VALUES ('$name', '$surname', '$birth_date', '$gender', '$birth_city')";
             $this->conn->exec($sql);
         }
     }
 
-    public function newPeople($name = null, $surname = null, $birth_date = null, $gender = null, $birth_city = null)
+    public function addNewPeople($name = null, $surname = null, $birth_date = null, $gender = null, $birth_city = null)
     {
         $sql = "INSERT INTO `user`(`name`, `surname`, `birth_date`, `gender`, `birth_city`) 
         VALUES ('$name', '$surname', '$birth_date', '$gender', '$birth_city')";
@@ -39,32 +46,35 @@ class DBPeople
         $this->conn->exec($sql);
     }
 
-    static function dateToAge($people){
+    static function dateToAge($people)
+    {
         $birth_date = strtotime($people->birth_date);
         $current_date = strtotime(date('y-m-d'));
         $age =floor(abs($birth_date - $current_date)/60/60/24/365);
         return $age;
     } 
 
-    static function getGender($people){
+    static function getGender($people)
+    {
         $gender = $people->gender;
-        if ($gender == 0){
+        if ($gender == 0) {
             return 'Male';
         } else {
             return 'Female';
         }
     } 
 
-    public function formatPeople($isGender = null, $isDate = null){
+    public function formatPeople($isGender = null, $isDate = null)
+    {
         $newStd = new stdClass;
         $newStd->name = $this->name;
         $newStd->surname = $this->surname;
-        if ($isDate != null){
+        if ($isDate != null) {
             $newStd->age = $this::dateToAge($this);
         } else {
             $newStd->birth_date = $this->birth_date;
         };
-        if ($isGender != null){
+        if ($isGender != null) {
             $newStd->gender = $this::getGender($this);
         } else {
             $newStd->gender = $this->gender;
--- second_class.php
+++ second_class.php
@@ -5,30 +5,37 @@ include_once "index.php";
 
 class PeopleList
 {
+    /**Данный класс взаимодействует с классом BDPeople и таблицей user.
+     * Он может отбирать людей по id и создавать из полученного отбора массив,
+     * состоящий из экземпляров класса BDPeople и удалять их из БД.
+     */
     private $id;
 
-    public function  __construct($expression){
+    public function  __construct($expression)
+    {
         $column = "user_".$expression;
         $conn = new PDO("mysql:host=127.0.0.1;dbname=dbuser", "root", "");
         $sql = "SELECT * FROM `user` WHERE $column";
         $people = $conn->query($sql);
         $searchId = array();
-        while($row = $people->fetch()){
+        while ($row = $people->fetch()) {
         $this->searchId[] = $row['user_id'];
         }
         
     }
 
-    public function peopleById(){
+    public function getPeopleById()
+    {
         $allPeople = array();
-        foreach($this->searchId as $value){
+        foreach ($this->searchId as $value) {
             $allPeople[] = new DBPeople($value);
         }
         return $allPeople;
     }
 
-    public function deletePeopleById(){
-        foreach($this->searchId as $value){
+    public function deletePeopleById()
+    {
+        foreach($this->searchId as $value) {
             $people = new DBPeople($value);
             $people->deletePeople();
         }
